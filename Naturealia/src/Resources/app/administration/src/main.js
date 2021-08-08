82951e27fa4a0d9290f02b038be9668b2a6ec852
Igor Kovalenko <kigor2152@gmail.com>
2021-08-08 13:06:48 +0300

    add new element to the admin panel

--- Naturealia/src/Resources/app/administration/src/main.js
+++ Naturealia/src/Resources/app/administration/src/main.js
@@ -1,3 +1,4 @@
 import './module/sw-cms/blocks/text-image/image-text-gallerys';
 import './module/sw-cms/blocks/commerce/product-four-column';
 import './module/sw-cms/blocks/text-image/image-text-row-two';
+import './module/sw-cms/blocks/text-image/image-text-row-twos';
